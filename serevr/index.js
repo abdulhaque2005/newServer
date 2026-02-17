7a66d7cd81fb28886598900b26b36a4b0ed7ea11
abdulhaque2005 <abdulhaquecg@gmail.com>
2026-02-17 16:16:05 +0530

    updated

--- serevr/index.js
+++ serevr/index.js
@@ -1,8 +1,8 @@
-
-
 const express = require("express");
+const cors =require('cors')
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const user = [
     { "attendence": 96, "uid": 108444, "sub": 14, "bonus": 20, name: "amit" },
